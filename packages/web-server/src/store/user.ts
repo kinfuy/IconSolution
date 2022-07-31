8805b8428873d73ba2654b205aa6e7d4366605bc
9listen <xiongzzero@outlook.com>
2022-07-31 15:08:52 +0800

    feat: fixed

--- packages/web-server/src/store/user.ts
+++ packages/web-server/src/store/user.ts
@@ -1,12 +1,13 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
-
+//interface和type一样 声明一个类型
 interface UserInfo {
   id: string;
   email: string;
   name: string;
   avator: string;
 }
+// 和组件里面setup一样的用法，只是库定义不一样
 export const useUserStore = defineStore('user', () => {
   const userInfo = ref({
     id: '',
@@ -14,11 +15,11 @@ export const useUserStore = defineStore('user', () => {
     name: '',
     avator: ''
   });
-  //
+
   function setUserinfo(user: UserInfo) {
+    //user遵循userinfo的类型定义
     userInfo.value = user;
   }
-
   return {
     userInfo,
     setUserinfo
